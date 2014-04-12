1e05b2b59b41093755cdcec647ec1ae8772b9658
Belmar Santanilla Gutiérrez <belmars@gmail.com>
2014-04-12 14:08:10 -0500

    Se agrega el componente social debajo del título de los artículos.

--- public/themes/digitalcocktail/post.blade.php
+++ public/themes/digitalcocktail/post.blade.php
@@ -11,6 +11,26 @@
 @section('content')
 	<section>
 		<h2 class="title">{{ $post->title }}</h2>
+		<!-- Twitter -->
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="{{ url('/servicios') }}" data-lang="es">Twittear</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		<!-- Facebook -->		
+		<div class="fb-like" data-href="http://digitalcocktail.co" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false"></div>
+
+		<!-- Google Plus -->
+		<div class="g-plusone" data-size="tall"></div>
+
+		<!-- Place this tag after the last +1 button tag. -->
+		<script type="text/javascript">
+		  window.___gcfg = {lang: 'es-419'};
+
+		  (function() {
+		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		    po.src = 'https://apis.google.com/js/platform.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		  })();
+		</script>		
+	</section>
 
 		{{ $post->parsed_content }}
 
